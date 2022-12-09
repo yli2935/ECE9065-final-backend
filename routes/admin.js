049bdabb13aaf7b61801a6d9870e2bb62958a9b2
Li <yli2935@uwo.ca>
2022-12-09 15:16:18 -0500

    change admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@
  * @Author: Li yli2935@uwo.ca
  * @Date: 2022-11-26 14:14:14
  * @LastEditors: Li yli2935@uwo.ca
- * @LastEditTime: 2022-12-09 09:55:19
+ * @LastEditTime: 2022-12-09 15:15:57
  * @FilePath: /ECE9065-final-backend/routes/admin.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,9 +16,9 @@ router.put("/userenable/:email", adminController.enableUser);
 router.put("/usersetadmin/:email", adminController.setUserAsAdmin);
 router.put("/usercanceladmin/:email", adminController.cancelUserAsAdmin);
 
-router.put("/getAllUser",checkAdminAuth, adminController.getUserList);
+router.put("/getAllUser", adminController.getUserList);
 
-router.get("/getAllReview",checkAdminAuth, adminController.getAllReview);
+router.get("/getAllReview", adminController.getAllReview);
 
 module.exports = router;
 
